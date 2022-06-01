3a2d7f1ac8742511350ec8b45bc8b35415830f4a
Daniel R <daniel_reyes@javeriana.edu.co>
2022-05-31 19:26:11 -0500

    Completed conection & managed Pattern dependencies

--- ApiEndpoint/src/main/java/com/rest/ws/RestApi.java
+++ ApiEndpoint/src/main/java/com/rest/ws/RestApi.java
@@ -3,15 +3,25 @@
  */
 package com.rest.ws;
 
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.naming.NamingException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 import com.controller.rst.ControladorSrv;
 
@@ -23,27 +33,46 @@ import com.controller.rst.ControladorSrv;
 public class RestApi {
 
 	/**
+	 * @param uriInfo 
 	 * @throws NamingException 
 	 * 
 	 */	
+	
 	@GET
+	@Path("{auth}")
 	@Produces(MediaType.TEXT_PLAIN)
-	public String hello() throws NamingException {
-		//return new ControladorSrv().traerUsuariosInd();
-		return "hello";
+	public String hello(@PathParam("auth") String auth, @QueryParam("user") String name, @Context UriInfo uriInfo) throws NamingException {
+		
+		MultivaluedMap<String, String> queryParams = uriInfo.getQueryParameters();
+
+		String from = uriInfo.getQueryParameters().getFirst("user");
+		    // In case you want to get the whole generated string
+		    String query = uriInfo.getRequestUri().getQuery();
+
+		    String output = "QueryParams: " + queryParams 
+		            + "<br> Keys: " + queryParams.keySet() 
+		            + "<br> Values: " + queryParams.values()
+		            + "<br> Query: " + query;
+		
+		return new ControladorSrv().traerUsuariosInd();
+		
+		// return name;
 	}
+
+
 	/*
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response */
 	
-	
+	/*
 	@GET
+	@Path("{auth}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public String getUsr() throws NamingException{
 	
 		return new ControladorSrv().traerUsuariosInd();
 		
-	}
+	}*/
 
 }
--- ApiGateway/src/main/java/com/rest/ws/RestApi.java
+++ ApiGateway/src/main/java/com/rest/ws/RestApi.java
@@ -3,13 +3,24 @@
  */
 package com.rest.ws;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.naming.NamingException;
+import javax.security.auth.message.callback.PrivateKeyCallback.Request;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+
+import org.glassfish.jersey.client.ClientConfig;
 
 import com.controller.rst.ControladorSrv;
 
@@ -28,18 +39,31 @@ public class RestApi {
 
 	/**
 	 * 
-	 */	
+	 */	/*
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
-	public String hello() {
+	public String hello(@Context HttpHeaders httpheaders) {
 		//return new ControladorSrv().agregarUsuario();
-		return "hello";
-	}
+		String token = httpheaders.getHeaderString("token");
+		return token;
+	}*/
+	
+
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public com.rest.ws.Usuario getUsr() throws NamingException{
-	
+		
+		
+	    //JSONObject jsonObj = new JSONObject(token);
+		//ClientConfig config = new ClientConfig();
+		//Client client = ClientBuilder.newClient(config);
+		//WebTarget target = client.target(null);
+		
+		//String userName = request.getParameter("username");
+		//String password = request.getParameter("pass");
+		
+		
 		return new ControladorSrv().traerUsuariosInd("colio", "56f");
 		
 	}
